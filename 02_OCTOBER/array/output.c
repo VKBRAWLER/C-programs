3742f4e1e7500ecd20cf419cb058fa1f1482f01e
Varun Kharkwal <varunkh12345@gmail.com>
2023-10-02 14:06:06 +0530

    added

--- 02_OCTOBER/array/output.c
+++ 02_OCTOBER/array/output.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
- int array[3][4] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
- printf("%u %u %u\n", array[0]+1, *(array[0]+1), *(*(array+0)+1));
+ int a[2][3][4] = { {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 2}, {2, 1, 4, 7, 6, 7, 8, 9, 0, 0, 0, 0} };
+ printf("%u, %u, %u, %d\n", a, *a, **a, ***a);
  return 0;
-}
+} //addresses are arbitrarily taken
