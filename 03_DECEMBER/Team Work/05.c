dc2a25a0ac9d5af31d52874fcd64eef2c6b153c1
Varun Kharkwal <varunkh12345@gmail.com>
2023-12-08 09:26:52 +0530

    new program #1

---
+++ 03_DECEMBER/Team Work/05.c
@@ -0,0 +1,42 @@
+// Write a C program to craeate a single linked list , like L0 -> L1 -> … -> Ln-1 -> Ln. Write another C fucntion to rearrange the nodes in the list so that the new formed list is : L0 -> Ln -> L1 -> Ln-1 -> L2 -> Ln-2 .You are required to do this in place without altering the nodes’ values.
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node {
+  int data; struct Node* next;
+} Node;
+void InsertData(Node** L) {
+  Node *new = (Node*) malloc(sizeof(Node));
+  printf("Enter data: ");
+  scanf("%d", &new->data);
+  new->next = NULL;
+  if (!*L) { *L = new; return; }
+  Node *p = *L;
+  while(p->next != NULL) { p = p->next; }
+  p->next = new;
+}
+void InsertNode(Node** L, Node* node) {
+  if (!*L) { *L = node; return; }
+  Node *p = *L;
+  while(p->next != NULL) { p = p->next; }
+  node->next = NULL;
+  p->next = node;
+}
+void Display(Node* L) {
+  for (Node* p = L; p != NULL; p = p->next) { printf(" %d ->", p->data); }
+  printf(" NULL\n");
+}
+int main() {
+  Node *L = NULL, *temp = NULL, *rear = NULL;
+  int num;
+  printf("Enter the length of linked list: ");
+  scanf("%d", &num);
+  for (int i = 0; i < num; i++) { InsertData(&L); }
+   int c = 1;
+  for (Node* p = L; p != NULL; c++) {
+    Node* next = p->next;
+    printf("%d", next);
+    InsertNode(&temp, p);
+    p = next;
+  }
+  Display(temp);
+}
